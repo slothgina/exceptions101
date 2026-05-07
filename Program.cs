601339cd0262fa58128aaa64548124ec8fb3b12c
slothgina <gina.slothstudio@zohomail.com>
2026-05-07 19:56:07 -0400

    Access the properties of an exception object Catch a specific exception type
    Catch multiple exceptions in a code block
    he catch clause should be configured to catch a specific exception type. For example, the DivideByZeroException exception type.
    The properties of an exception object can be accessed within the catch block. For example, you can use the Message property to inform the application user of an issue.
    You can specify two or more catch clauses when you need to catch more than one exception type.

--- Program.cs
+++ Program.cs
@@ -15,11 +15,10 @@ static void Process1()
     {
         WriteMessage();
     }
-    catch
+    catch (DivideByZeroException ex)
     {
-        Console.WriteLine("Exception caught in Process1");
+        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
     }
-
 }
 
 static void WriteMessage()
@@ -28,7 +27,19 @@ static void WriteMessage()
     double float2 = 0.0;
     int number1 = 3000;
     int number2 = 0;
+    byte smallNumber;
 
-    Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    try
+    {
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+    }
+    checked
+    {
+        smallNumber = (byte)number1;
+    }
 }
